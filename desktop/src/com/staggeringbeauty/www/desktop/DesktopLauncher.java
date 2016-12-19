5f0e5f08bdea0cc9bc9ab422de0b16aed8fc48b2
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2016-12-19 12:53:15 -0500

    Working on Summative Project

---
+++ desktop/src/com/staggeringbeauty/www/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.staggeringbeauty.www.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.staggeringbeauty.www.SummativeProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SummativeProject(), config);
+	}
+}
