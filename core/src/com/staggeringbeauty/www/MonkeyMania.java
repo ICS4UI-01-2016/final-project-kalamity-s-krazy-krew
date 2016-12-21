d25490762e581c2450395aebad6646090d92c239
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2016-12-21 12:50:18 -0500

    Working on Summative Project

--- core/src/com/staggeringbeauty/www/MonkeyMania.java
+++ core/src/com/staggeringbeauty/www/MonkeyMania.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SummativeProject extends ApplicationAdapter {
+public class MonkeyMania extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
--- desktop/src/com/staggeringbeauty/www/desktop/DesktopLauncher.java
+++ desktop/src/com/staggeringbeauty/www/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.staggeringbeauty.www.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.staggeringbeauty.www.SummativeProject;
+import com.staggeringbeauty.www.MonkeyMania;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SummativeProject(), config);
+		new LwjglApplication(new MonkeyMania(), config);
 	}
 }
