89dabed5221cedffe4f01ee3280be550903f42af
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2016-12-22 13:37:56 -0500

    Working on Summative Project

--- desktop/src/com/staggeringbeauty/www/desktop/DesktopLauncher.java
+++ desktop/src/com/staggeringbeauty/www/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.staggeringbeauty.www.MonkeyMania;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.width = MonkeyMania.WIDTH;
+                config.height = MonkeyMania.HEIGHT;
 		new LwjglApplication(new MonkeyMania(), config);
 	}
 }
