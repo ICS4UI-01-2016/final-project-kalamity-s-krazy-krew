17c574baf240763d8f398454ebe9a29c4e8143a6
malcr1272 <malcr1272@HRH-IC0027342.wrdsb.ca>
2017-01-10 14:13:34 -0500

    Monkey Class

--- core/src/com/staggeringbeauty/www/Monkey.java
+++ core/src/com/staggeringbeauty/www/Monkey.java
@@ -4,16 +4,60 @@
  */
 package com.staggeringbeauty.www;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
 /**
  *
- * @author farrb0382
+ * @author malcr1272
  */
 public class Monkey {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private Vector3 position;
+    private Vector3 velocity;
+    private Texture monkeypic;
+    private Rectangle hitbox;
+    
+    private final float gravity = -10;
+    private final float movement = 100;
+    
+    public Monkey(int x, int y){
+       position = new Vector3 (x,y,0);
+       velocity = new Vector3 (movement,0,0);
+       //monkeypic = new Texture();
+       hitbox = new Rectangle(position.x, position.y, monkeypic.getWidth(), monkeypic.getHeight());
+    }
+    
+    public void bounce(){
+        velocity.y = 300;
+    }
+    
+    public void update(float deltaTime){
+        //add gravity
+        velocity.y += gravity;
+        //scale the velocity
+        velocity.scl(deltaTime);
+        //add velocity
+        position.add(velocity);
+        //unscale the velocity
+        velocity.scl(1/deltaTime);
+        //set hitbox
+        hitbox.setPosition(position.x, position.y);       
+    }
+    public void render(SpriteBatch batch){
+        batch.draw(monkeypic, gravity, gravity);
+    }
+    public void dispose(){
+        monkeypic.dispose();
+    }
+    public float getX(){
+        return position.x;
+    }
+    public float getY(){
+        return position.y;
+    }
+    public Rectangle getHitBox(){
+        return hitbox;
     }
 }
