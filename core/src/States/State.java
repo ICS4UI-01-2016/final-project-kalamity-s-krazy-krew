1917476d7ea2b7825789e7fc26566807d7bd1f8d
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2017-01-16 14:00:05 -0500

    Working on Summative Project

---
+++ core/src/States/State.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ *
+ * @author farrb0382
+ */
+public class State {
+    
+    private OrthographicCamera cam;
+//    private StateManager stateManager;
+    
+    
+}
