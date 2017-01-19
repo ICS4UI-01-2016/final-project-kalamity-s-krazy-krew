7b81bef6b2217005c8e071c84fe185582218a522
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2017-01-19 12:43:42 -0500

    Working on Summative Project

--- core/src/States/menuState.java
+++ core/src/States/menuState.java
@@ -19,11 +19,10 @@ import com.mygdx.game.MyGdxGame;
 public class menuState extends State {
 
     //private textures
-    private Texture background;
+    private Texture bg;
     private Texture button;
-    private int highScore;
     private BitmapFont font;
-
+    
     //menu state constructor
     public menuState(StateManager gsm) {
         super(gsm);
@@ -31,9 +30,6 @@ public class menuState extends State {
         //button = new Texture();
         setCameraView(MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
         setCameraPosition(getViewWidth() / 2, getViewHeight() / 2);
-
-        Preferences pref = Gdx.app.getPreferences("highscore");
-        highScore = pref.getInteger("highscore", 0);
 
         //default font
         font = new BitmapFont();
@@ -43,31 +39,24 @@ public class menuState extends State {
     public void render(SpriteBatch batch) {
         batch.setProjectionMatrix(getCombinedCamera());
         batch.begin();
-        font.draw(batch, "" + highScore, getViewWidth() / 2, getViewHeight() - 100);
-        batch.draw(background, 0, 0, getViewWidth(), getViewHeight());
+        batch.draw(bg, 0, 0, getViewWidth(), getViewHeight());
         batch.draw(button, getViewWidth() / 2 - button.getWidth() / 2, getViewHeight() / 2);
         batch.end();
     }
 
-    public void updateScore() {
-        Preferences pref = Gdx.app.getPreferences("highscore");
-        highScore = pref.getInteger("highscore", 0);
-    }
-
     @Override
     public void update(float deltaTime) {
-
     }
 
     @Override
     public void handleInput() {
         if (Gdx.input.justTouched()) {
+
             Vector3 touch = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+
             unproject(touch);
-            float buttonX = getViewWidth() / 2 - button.getWidth() / 2;
-            float buttonY = getViewHeight() / 2;
-            if (touch.x > buttonX && touch.x < button.getWidth()
-                    && touch.y > buttonY && touch.y < buttonY + button.getHeight()) {
+
+            if (touch.x <= MyGdxGame.HEIGHT && touch.y <= MyGdxGame.WIDTH) {
                 StateManager gsm = getStateManager();
                 gsm.push(new PlayState(gsm));
             }
@@ -76,7 +65,7 @@ public class menuState extends State {
 
     @Override
     public void dispose() {
-        background.dispose();
+        bg.dispose();
         button.dispose();
     }
 }
