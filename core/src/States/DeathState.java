832cec4f00a7adef06777f081573cc2c879fe6a2
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2017-01-25 12:03:56 -0500

    Death screen is working

--- core/src/States/DeathState.java
+++ core/src/States/DeathState.java
@@ -15,22 +15,20 @@ import com.mygdx.game.MyGdxGame;
  * @author farrb0382
  */
 public class DeathState extends State {
-    
+
     private Texture death;
     private Texture menu;
-    private Texture replay;
     private float CamY;
-    
+
     public DeathState(StateManager gsm) {
 
         super(gsm);
 
         death = new Texture("death.png");
         menu = new Texture("menu.png");
-        replay = new Texture("replay.png");
         setCameraView(MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
         setCameraPosition(getViewWidth() / 2, getViewHeight() / 2);
-        
+
     }
 
     @Override
@@ -38,7 +36,7 @@ public class DeathState extends State {
         batch.setProjectionMatrix(getCombinedCamera());
         batch.begin();
         batch.draw(death, 0, 0, getViewWidth(), getViewHeight());
-        batch.draw(menu, getViewWidth() / 2 - menu.getWidth() / 2, getViewHeight() / 2 - 150);
+        batch.draw(menu, getViewWidth() / 2 - menu.getWidth() / 2, getViewHeight() / 2 - 100);
         batch.end();
     }
 
@@ -48,17 +46,19 @@ public class DeathState extends State {
 
     @Override
     public void handleInput() {
-        
+
         if (Gdx.input.justTouched()) {
 
             Vector3 touch = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
 
             unproject(touch);
 
-            float buttonX = getViewWidth() / 2 - death.getWidth() / 2;
-            float buttonY = getViewHeight() / 2;
-            
-            if (touch.x < MyGdxGame.WIDTH && touch.y < MyGdxGame.HEIGHT) {
+            float buttonX = getViewWidth() / 2 - menu.getWidth() / 2;
+            float buttonY = getViewHeight() / 2 - 100;
+
+            if (touch.x > buttonX && touch.x < buttonX + menu.getWidth()
+                    && touch.y > buttonY && touch.y < buttonY + menu.getHeight()) {
+
                 StateManager gsm = getStateManager();
                 gsm.push(new menuState(gsm));
             }
@@ -68,18 +68,6 @@ public class DeathState extends State {
     @Override
     public void dispose() {
         death.dispose();
+        menu.dispose();
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
